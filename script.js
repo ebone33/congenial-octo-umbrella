58919bb39910780708e10d9be182e48f2b5c9bd9
Eboné <eliggins@protonmail.com>
2025-05-06 13:51:14 -0400

    done

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 // When you click on the search button a message appears//
-document.getElementById("searchbutton").addEventListener("click", myFunction);
-
-function myFunction() {
-    document.getElementById("searchbutton").innerHTML = "Go to www.chewy.com!";
-}
+document.getElementById("searchbutton").addEventListener("click", function(){alert ("Go to www.chewy.com!");
+});
 
 //the buttons on the dog and cats pages change color when you click them//
+const myButton = document.getElementById("add");
+myButton.addEventListener("click", function() {
+  this.style.backgroundColor = "green";
+});
 
-const button = document.querySelector("add");
-
-button.addEventListener("click", function () {
-        button.style.color = "green";
-    });
-
-//Validate that text has been entered into the search bar//
+//Validate that text has been entered into the search bar - this does not work because I have the alert above//
 
 function validateForm() {
-    let x = document.forms["search bar"].value;
+    let x = document.forms["search bar"]["text"].value;
     if (x == "") {
       alert("Please enter text.");
       return false;
